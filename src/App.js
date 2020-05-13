31b8291ea7b1ebc7831e1318865a59f197d3802c
ASand-design <59930866+ASand-design@users.noreply.github.com>
2020-05-13 13:39:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from './components/Input';
+import Task from './components/Task';
 import './App.css';
 
 function App() {
+  const [list, setList] = useState([]);
+
   return (
-    <div className="App">
-   
+    <div className= "App container" style={{width: '800px'}}>
+      {list.map((task, i) => (
+        <Task task= {task} setList= {setList} index= {i} list= {list} />
+      ))}
+      <Input list= {list} setList={setList} />
     </div>
   );
 }
